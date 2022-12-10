0cc6901cd57beea54647f68314efb51a790a1252
Edvard Englund <edvard.englund@medsci.uu.se>
2022-12-10 15:12:38 +0100

    implement telescope history

--- lua/mytelescope.lua
+++ lua/mytelescope.lua
@@ -4,8 +4,8 @@ require('telescope').setup {
   defaults = {
       mappings = {
           i = {
----              ["<C-n>"] = actions.move_selection_next,
----              ["<C-p>"] = actions.move_selection_previous,
+              ["<C-j>"] = require('telescope.actions').cycle_history_next,
+              ["<C-k>"] = require('telescope.actions').cycle_history_prev,
           }
       }
   },
