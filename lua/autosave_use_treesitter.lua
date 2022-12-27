1a3bb752fed6896c60a8e22b202bd09e40b9e955
Edvard Englund <edvard.englund@medsci.uu.se>
2022-12-27 14:21:42 +0100

    clean up

--- lua/autosave_use_treesitter.lua
+++ lua/autosave_use_treesitter.lua
@@ -7,24 +7,6 @@ local transform_line = function(line)
     return line:gsub('%s*[%[%(%{]*%s*$', '')
 end
 
-local get_line_for_node = function(node, type_patterns, transform_fn)
-    local node_type = node:type()
-    local is_valid = false
-    for _, rgx in ipairs(type_patterns) do
-        if node_type:find(rgx) then
-            is_valid = true
-            break
-        end
-    end
-    if not is_valid then return '' end
-    -- print("node repr: " .. vim.inspect(vim.trim(node:field("type"))))
-    print("node repr: " .. vim.inspect(vim.trim(vim.inspect(node:field("name")))))
-    -- print("get_node_text: " .. vim.inspect(vim.trim(query.get_node_text(node, vim.api.nvim_get_current_buf()) or '')))
-    local line = transform_fn(vim.trim(query.get_node_text(node) or ''))
-    -- Escape % to avoid get_method_name to evaluate content as expression
-    return line:gsub('%%', '%%%%')
-end
-
 local matches_pattern = function(node, type_patterns)
     local node_type = node:type()
     local is_valid = false
@@ -44,15 +26,10 @@ name: (identifier) @name)
 )
 ]]
 
-local find_position = function(start_node, bufnr, query_string)
+local get_node_text = function(start_node, bufnr, query_string)
     local query = ts.parse_query("c_sharp", query_string)
     for id, node in query:iter_captures(start_node, bufnr, 0, -1) do
         if id == 1 then
-            local start = { node:start() }
-            local start_byte = start[3]
-            local end_ = { node:end_() }
-            local end_byte = end_[3]
-            -- return { start_byte, end_byte }
             return query_module.get_node_text(node, bufnr)
         end
     end
@@ -62,7 +39,7 @@ end
 function get_method_name(bufnr)
     local options = {}
     local indicator_size = 100
-    local type_patterns = {'class', 'function', 'method'}
+    local type_patterns = {'class', 'method'}
     local transform_fn = transform_line
     local separator = ' -> '
 
@@ -80,8 +57,8 @@ function get_method_name(bufnr)
         expr = expr:parent()
     end
     local method_node = expr
-    local pos = find_position(method_node, bufnr, query_string)
-    return pos
+    local text = get_node_text(method_node, bufnr, query_string)
+    return text
 end
 
 local group = vim.api.nvim_create_augroup("edvard-automagic", { clear = true })
@@ -97,7 +74,7 @@ local attach_to_buffer = function(bufnr)
     } )
 end
 
-vim.api.nvim_create_user_command("ShowSemantic", function()
+vim.api.nvim_create_user_command("GenerateTestArguments", function()
     attach_to_buffer(vim.api.nvim_get_current_buf())
 end, {})
 
