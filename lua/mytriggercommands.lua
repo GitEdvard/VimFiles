6aa382cb1647e7fcb158be45b44a8c3cfc554b9f
Edvard Englund <edvard.englund@medsci.uu.se>
2023-01-10 15:39:47 +0100

    Working on trigger commands

--- lua/mytriggercommands.lua
+++ lua/mytriggercommands.lua
@@ -5,4 +5,30 @@ local trigger_command = function()
     require'trigger-commands'.run_single(run_settings)
 end
 local opts = { noremap = true, silent = true }
-vim.keymap.set('n', '<leader>u', trigger_command, opts)
+vim.keymap.set('n', '<leader>us', trigger_command, opts)
+
+local run_settings = require'read-settings'.read_json('.runrestsettings.json')
+local trigger_command = function()
+    require'trigger-commands'.run_rest_call(run_settings)
+end
+vim.keymap.set('n', '<leader>ur', trigger_command, opts)
+
+local reset = function()
+    package.loaded['trigger-commands'] = nil
+    package.loaded['mytriggercommands'] = nil
+end
+vim.keymap.set('n', '<leader>uc', reset)
+
+-- local hello = function()
+--     print('hello')
+-- end
+
+-- local trigger_group = vim.api.nvim_create_augroup('my-trigger-commands', { clear = true })
+
+-- vim.cmd("autocmd User myevent echo 'hello'")
+-- vim.api.nvim_create_autocmd('User', {
+--     pattern = 'myevent',
+--     group = trigger_group,
+--     command = 'lua hello()'
+-- })
+
