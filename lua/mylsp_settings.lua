9a5627338b27f1275760bdef22bc1e7579dd7d3f
Edvard Englund <edvard.englund@medsci.uu.se>
2023-01-31 11:00:25 +0100

    Add language server for angular

--- lua/mylsp_settings.lua
+++ lua/mylsp_settings.lua
@@ -64,3 +64,26 @@ vim.api.nvim_create_autocmd({ "BufWritePost" }, {
         require("lint").try_lint()
     end,
 })
+
+local cwd = vim.fn.getcwd()
+local project_library_path = cwd .. "/node_modules"
+
+local cmd = {
+    cwd .. "/node_modules/@angular/language-server/bin/ngserver",
+    "--ngProbeLocations",
+    project_library_path,
+    "--tsProbeLocations",
+    project_library_path ,
+    "--stdio",
+}
+
+require'lspconfig'.angularls.setup{
+    cmd = cmd,
+    on_new_config = function(new_config, new_root_dir)
+        new_config.cmd = cmd
+    end
+}
+
+-- require'lspconfig'.tsserver.setup{
+--     cmd = { "node /home/edvard/.nvm/versions/node/v14.16.0/lib/node_modules/typescript/bin/tsserver"}
+-- }
