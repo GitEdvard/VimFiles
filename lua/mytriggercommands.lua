3e231297cd5ba4a904d093211b28f6694fc9b053
Edvard Englund <edvard.englund@medsci.uu.se>
2023-02-02 09:13:54 +0100

    Clean up triggercommands

--- lua/mytriggercommands.lua
+++ lua/mytriggercommands.lua
@@ -1,22 +1,22 @@
 -- :lua package.loaded["trigger-commands"] = nil
-local run_settings = require'read-settings'.read_json('.dapsettings.json')
-local trigger_command = function()
+local run_settings = require'read-settings'.read_json('.runsettings.json')
+local trigger_command_single = function()
     vim.cmd("let $CURRENT_PY_PATH = substitute(expand('%:r'), '/', '.', 'g')")
     require'trigger-commands'.run_single(run_settings)
 end
 local opts = { noremap = true, silent = true }
-vim.keymap.set('n', '<leader>us', trigger_command, opts)
 
 local run_settings = require'read-settings'.read_json('.runrestsettings.json')
-local trigger_command = function()
+local trigger_command_rest = function()
     require'trigger-commands'.run_rest_call(run_settings)
 end
-vim.keymap.set('n', '<leader>ur', trigger_command, opts)
 
 local reset = function()
     package.loaded['trigger-commands'] = nil
     package.loaded['mytriggercommands'] = nil
 end
+vim.keymap.set('n', '<leader>us', trigger_command_single, opts)
+vim.keymap.set('n', '<leader>ur', trigger_command_rest, opts)
 vim.keymap.set('n', '<leader>uc', reset)
 
 -- local hello = function()
