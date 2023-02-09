50abdc955dd5687dfc9a6b39c7a2a35e0178a827
Edvard Englund <edvard.englund@medsci.uu.se>
2023-02-09 10:03:57 +0100

    New key binding for insert block

--- common.vim
+++ common.vim
@@ -147,7 +147,7 @@ nnoremap gf <C-w>v<C-w>T:e <cfile><CR>
 
 nnoremap <leader>j :UndotreeShow<cr> :UndotreeFocus<cr>
 nnoremap <leader>k i<cr><esc>O
-inoremap <leader>k <cr><esc>O
+inoremap <c-k> <cr><esc>O
 " Reload current file
 nnoremap <leader>e :e!<CR>
 nnoremap <leader>Q :bufdo bdelete<cr>
--- lua/luasnip/myluasnip_settings.lua
+++ lua/luasnip/myluasnip_settings.lua
@@ -18,17 +18,17 @@ ls.config.set_config {
   },
 }
 
-vim.keymap.set({ 'i', 's' }, '<c-k>', function()
+vim.keymap.set({ 'i', 's' }, '<c-j>', function()
   if ls.expand_or_jumpable() then
     ls.expand_or_jump()
   end
 end)
 
-vim.keymap.set({ 'i', 's' }, '<c-j>', function()
-  if ls.jumpable(-1) then
-    ls.jump(-1)
-  end
-end, { silent = true })
+-- vim.keymap.set({ 'i', 's' }, '<c-j>', function()
+--   if ls.jumpable(-1) then
+--     ls.jump(-1)
+--   end
+-- end, { silent = true })
 
 vim.keymap.set({ 'i', 's' }, '<c-l>', function()
   if ls.choice_active() then
