75e215c39c49c1f0ce331958cee86e76bba9eae6
Edvard Englund <edvard.englund@medsci.uu.se>
2023-02-12 18:18:39 +0100

    remove create java dap config command

--- lua/java/myjava_settings.lua
+++ lua/java/myjava_settings.lua
@@ -9,4 +9,3 @@ end
 local opts = { noremap = true, silent = true }
 vim.keymap.set('n', '<leader>ub', build, opts)
 vim.keymap.set('n', '<leader>ud', package, opts)
-vim.keymap.set('n', '<leader>um', '<cmd>lua require(\'jdtls.dap\').setup_dap_main_class_configs()<cr>', opts)
