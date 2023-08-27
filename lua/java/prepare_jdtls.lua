9a6cea5181e8a9befae80efb658697306c24d0a9
Edvard Englund <edvard.englund_c@sandvik.com>
2023-08-27 14:30:16 +0200

    Implement non-sync execution of hide/unhide

--- lua/java/prepare_jdtls.lua
+++ lua/java/prepare_jdtls.lua
@@ -11,7 +11,7 @@ local transpose2 = function (aStr, injectStr, secondArg)
   return ret
 end
 
-local expand_execute = function (command, strToExpand)
+local expand_execute_sync = function (command, strToExpand)
   local expandedStr = vim.fn.expand(strToExpand)
   local lines = mysplit(expandedStr, "\n")
   for _, v in ipairs(lines) do
@@ -19,8 +19,17 @@ local expand_execute = function (command, strToExpand)
   end
 end
 
+local expand_execute = function (command, strToExpand, commands)
+  local expandedStr = vim.fn.expand(strToExpand)
+  local lines = mysplit(expandedStr, "\n")
+  for _, v in ipairs(lines) do
+    table.insert(commands, transpose(command, v))
+  end
+  return commands
+end
+
 -- Expand strToExpand, and find all paths. Call command, second occurance is instead replaced with secondArg
-local expand_execute_mult = function (command, strToExpand, secondArg)
+local expand_execute_mult_sync = function (command, strToExpand, secondArg)
   local expandedStr = vim.fn.expand(strToExpand)
   local lines = mysplit(expandedStr, "\n")
   for _, v in ipairs(lines) do
@@ -32,6 +41,20 @@ local expand_execute_mult = function (command, strToExpand, secondArg)
   end
 end
 
+-- Expand strToExpand, and find all paths. Call command, second occurance is instead replaced with secondArg
+local expand_execute_mult = function (command, strToExpand, secondArg, commands)
+  local expandedStr = vim.fn.expand(strToExpand)
+  local lines = mysplit(expandedStr, "\n")
+  for _, v in ipairs(lines) do
+    local parts = mysplit(v, "\\")
+    table.remove(parts, #parts)
+    local folder = table.concat(parts, "\\")
+    local destPath = folder .. "\\".. secondArg
+    table.insert(commands, transpose2(command, v, destPath))
+  end
+  return commands
+end
+
 M.test = function()
   -- expand_execute("Git update-index --assume-unchanged {}", "*/.project")
   -- local cmd = "mycommand {}"
@@ -40,34 +63,77 @@ M.test = function()
   -- print(transpose(cmd2, "hejdaa"))
 end
 
-M.hide_jdtls_files = function()
+M.test_multi_line = function()
+  local cmd = [[echo "hello1"
+  echo "hello2"]]
+  require'trigger-commands'.run_single( cmd )
+end
+
+M.hide_jdtls_files_sync = function()
   print("Prepare to use jdtls server ...")
   vim.cmd("Git update-index --assume-unchanged .project")
   vim.cmd("Git update-index --assume-unchanged pom.xml")
   -- vim.cmd("!copy /y i290.cmm\\tom.xml i290.cmm\\pom.xml")
-  expand_execute("Git update-index --assume-unchanged {}", "*/.project")
-  expand_execute("Git update-index --assume-unchanged {}", "*/pom.xml")
-  expand_execute("Git update-index --assume-unchanged {}", "*/.classpath")
-  expand_execute("Git update-index --assume-unchanged {}", "*/.settings/org.eclipse.jdt.core.prefs")
+  expand_execute_sync("Git update-index --assume-unchanged {}", "*/.project")
+  expand_execute_sync("Git update-index --assume-unchanged {}", "*/pom.xml")
+  expand_execute_sync("Git update-index --assume-unchanged {}", "*/.classpath")
+  expand_execute_sync("Git update-index --assume-unchanged {}", "*/.settings/org.eclipse.jdt.core.prefs")
   vim.cmd("!copy /y tom.xml pom.xml")
-  expand_execute_mult("!copy /y {} {}", "*/tom.xml", "pom.xml")
+  expand_execute_mult_sync("!copy /y {} {}", "*/tom.xml", "pom.xml")
+  print("Done")
+end
+
+M.unhide_jdtls_files_sync = function()
+  print("Restore files after jdtls usage ...")
+  expand_execute_sync("Git update-index --no-assume-unchanged {}", "*/.project")
+  expand_execute_sync("Git update-index --no-assume-unchanged {}", "*/pom.xml")
+  expand_execute_sync("Git update-index --no-assume-unchanged {}", "*/.classpath")
+  expand_execute_sync("Git update-index --no-assume-unchanged {}", "*/.settings/org.eclipse.jdt.core.prefs")
+  expand_execute_sync("Git checkout HEAD -- {}", "*/.project")
+  expand_execute_sync("Git checkout HEAD -- {}", "*/.classpath")
+  expand_execute_sync("Git checkout HEAD -- {}", "*/.settings/org.eclipse.jdt.core.prefs")
+  expand_execute_sync("Git checkout HEAD -- {}", "*/pom.xml")
+  vim.cmd("Git update-index --no-assume-unchanged .project")
+  vim.cmd("Git checkout HEAD -- .project")
+  vim.cmd("Git update-index --no-assume-unchanged pom.xml")
+  vim.cmd("Git checkout HEAD -- pom.xml")
+  print("Done")
+end
+
+M.hide_jdtls_files = function()
+  print("Prepare to use jdtls server ...")
+  local commands = {}
+  table.insert(commands, "git update-index --assume-unchanged .project")
+  table.insert(commands, "git update-index --assume-unchanged pom.xml")
+  -- vim.cmd("!copy /y i290.cmm\\tom.xml i290.cmm\\pom.xml")
+  commands = expand_execute("git update-index --assume-unchanged {}", "*/.project", commands)
+  commands = expand_execute("git update-index --assume-unchanged {}", "*/pom.xml", commands)
+  commands = expand_execute("git update-index --assume-unchanged {}", "*/.classpath", commands)
+  commands = expand_execute("git update-index --assume-unchanged {}", "*/.settings/org.eclipse.jdt.core.prefs", commands)
+  table.insert(commands, "copy /y tom.xml pom.xml")
+  commands = expand_execute_mult("copy /y {} {}", "*/tom.xml", "pom.xml", commands)
+  commands = table.concat(commands, "\n")
+  require'trigger-commands'.run_single( commands )
   print("Done")
 end
 
 M.unhide_jdtls_files = function()
   print("Restore files after jdtls usage ...")
-  expand_execute("Git update-index --no-assume-unchanged {}", "*/.project")
-  expand_execute("Git update-index --no-assume-unchanged {}", "*/pom.xml")
-  expand_execute("Git update-index --no-assume-unchanged {}", "*/.classpath")
-  expand_execute("Git update-index --no-assume-unchanged {}", "*/.settings/org.eclipse.jdt.core.prefs")
-  expand_execute("Git checkout HEAD -- {}", "*/.project")
-  expand_execute("Git checkout HEAD -- {}", "*/.classpath")
-  expand_execute("Git checkout HEAD -- {}", "*/.settings/org.eclipse.jdt.core.prefs")
-  expand_execute("Git checkout HEAD -- {}", "*/pom.xml")
-  vim.cmd("Git update-index --no-assume-unchanged .project")
-  vim.cmd("Git checkout HEAD -- .project")
-  vim.cmd("Git update-index --no-assume-unchanged pom.xml")
-  vim.cmd("Git checkout HEAD -- pom.xml")
+  local commands = {}
+  commands = expand_execute("git update-index --no-assume-unchanged {}", "*/.project", commands)
+  commands = expand_execute("git update-index --no-assume-unchanged {}", "*/pom.xml", commands)
+  commands = expand_execute("git update-index --no-assume-unchanged {}", "*/.classpath", commands)
+  commands = expand_execute("git update-index --no-assume-unchanged {}", "*/.settings/org.eclipse.jdt.core.prefs", commands)
+  commands = expand_execute("git checkout HEAD -- {}", "*/.project", commands)
+  commands = expand_execute("git checkout HEAD -- {}", "*/.classpath", commands)
+  commands = expand_execute("git checkout HEAD -- {}", "*/.settings/org.eclipse.jdt.core.prefs", commands)
+  commands = expand_execute("git checkout HEAD -- {}", "*/pom.xml", commands)
+  table.insert(commands, "git update-index --no-assume-unchanged .project")
+  table.insert(commands, "git checkout HEAD -- .project")
+  table.insert(commands, "git update-index --no-assume-unchanged pom.xml")
+  table.insert(commands, "git checkout HEAD -- pom.xml")
+  commands = table.concat(commands, "\n")
+  require'trigger-commands'.run_single( commands )
   print("Done")
 end
 
--- myplugfiles/trigger-commands.vim
+++ myplugfiles/trigger-commands.vim
@@ -1,5 +1,6 @@
 " Plug '/home/edvard/sources/admin/VimPlugins/trigger-commands.nvim'
-Plug 'GitEdvard/trigger-commands.nvim'
+" Plug 'GitEdvard/trigger-commands.nvim'
+Plug 'C:\Users\yh6032\HOME\myvim\trigger-commands.nvim'
 
 augroup trigger-commands-plug-event
     autocmd!
