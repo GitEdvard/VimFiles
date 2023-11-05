59d491d0c72ccc5cd5b867ac1ea27cee66b44aff
Edvard Englund <edvard.englund_c@sandvik.com>
2023-11-05 12:58:12 +0100

    Remap next and previous completion to c-j, c-k
    trigger snippet is c-u

--- lua/mycmp_settings.lua
+++ lua/mycmp_settings.lua
@@ -31,6 +31,8 @@ cmp.setup({
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-i>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.abort(),
+        ['<C-j>'] = cmp.mapping.select_next_item(),
+        ['<C-k>'] = cmp.mapping.select_prev_item(),
         ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     }),
     sources = cmp.config.sources({
