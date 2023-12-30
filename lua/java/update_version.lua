c6d692ffb704fbfb23337245d1804b5d9bba447f
Edvard Englund <edvard.englund_c@sandvik.com>
2023-12-30 08:54:01 +0100

    refer to launch.tmp on nvim-adpat repo

--- lua/java/update_version.lua
+++ lua/java/update_version.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 M.update_version = function()
-  local tmp_file = find_root_path() .. "\\" .. "launch.tmp"
+  local tmp_file = vim.g.nvim_adapt_root .. "/root/" .. "launch.tmp"
   print(tmp_file)
   local cmd = "C:\\ProgramData\\NX_BASE\\NX12\\GLOBAL\\APPS\\YAPP\\jre8\\bin\\javaw.exe -Dmaven.home=EMBEDDED -Dclassworlds.conf=${tmp_file} -Dmaven.multiModuleProjectDirectory=C:\\ProgramData\\YAPP\\git\\i290.manufacturing -Dfile.encoding=UTF-8 -classpath C:\\Users\\yh6032\\yappclipse\\plugins\\org.eclipse.m2e.maven.runtime_1.8.3.20180227-2135\\jars\\plexus-classworlds-2.5.2.jar org.codehaus.plexus.classworlds.launcher.Launcher -Pversion -B -Dtycho.mode=maven tycho-versions:set-version tycho-versions:update-pom -DnewVersion=${version}"
   cmd = cmd:gsub("${tmp_file}", tmp_file)
