46e6944cd78049752fbda40594e71c74999aee59
Edvard Englund <edvard.englund_c@sandvik.com>
2025-01-11 16:47:52 +0100

    Open indata file from config picker

--- lua/myrun_configs.lua
+++ lua/myrun_configs.lua
@@ -1,6 +1,7 @@
 local M = {}
 require("telescope").load_extension("config_picker")
 local L = require'java.launcher'
+local yapp_paths = require("java.yapp_paths")
 
 local config_picker = require("config-picker")
 
@@ -27,5 +28,19 @@ config_picker.on_config_selected(function(metadata)
   save_run_config(file_path)
   L.launch(file_path)
 end)
+
+config_picker.on_indata_open(function(metadata)
+  local file_path = 'run-configs/' .. metadata.text
+  local config_table = require'read-settings'.read_json(file_path)
+  local project_name = config_table.project
+  -- local parameter_files = yapp_paths.find_root_path() .. "\\" .. project_name .. "\\parameter-files\\"
+  local parameter_files = vim.fn.getcwd() .. "\\" .. project_name .. "\\parameter-files\\"
+  local indata_path = parameter_files .. config_table.pfile
+  vim.cmd('tabe ' .. indata_path)
+end)
+
+M.open_indata = function()
+  print("hej")
+end
 
 return M
