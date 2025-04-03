214c7b8d213fed9527407ced99b00f5d1e9d9a4b
Edvard Englund <edvard.englund@medsci.uu.se>
2025-04-03 10:32:50 +0200

    Use ruff linter for python

--- lua/lsp/python.lua
+++ lua/lsp/python.lua
@@ -8,11 +8,26 @@ M.setup = function(lsp_flags, capabilities, on_attach)
     settings = {
       pylsp = {
         plugins = {
-          flake8 = {enabled = true},
-          pycodestyle = {enabled = false},
-          pyflakes = {enabled = false},
-          pylint = {enabled = true},
-          mccabe = {enabled = false},
+          ruff = {
+            enabled = true,  -- Enable the plugin
+            formatEnabled = true,  -- Enable formatting using ruffs formatter
+            executable = "/home/edvard/.local/bin/ruff",  -- Custom path to ruff
+            -- config = "<path_to_custom_ruff_toml>",  -- Custom config for ruff to use
+            -- extendSelect = { "I" },  -- Rules that are additionally used by ruff
+            -- extendIgnore = { "C90" },  -- Rules that are additionally ignored by ruff
+            format = { "I" },  -- Rules that are marked as fixable by ruff that should be fixed when running textDocument/formatting
+            -- severities = { ["D212"] = "I" },  -- Optional table of rules where a custom severity is desired
+            unsafeFixes = false,  -- Whether or not to offer unsafe fixes as code actions. Ignored with the "Fix All" action
+
+            -- Rules that are ignored when a pyproject.toml or ruff.toml is present:
+            lineLength = 88,  -- Line length to pass to ruff checking and formatting
+            exclude = { "__about__.py" },  -- Files to be excluded by ruff checking
+            select = { "E" },  -- Rules to be enabled by ruff
+            -- ignore = { "D210" },  -- Rules to be ignored by ruff
+            perFileIgnores = { ["__init__.py"] = "CPY001" },  -- Rules that should be ignored for specific files
+            preview = false,  -- Whether to enable the preview style linting and formatting.
+            targetVersion = "py38",  -- The minimum python version to target (applies for both linting and formatting).
+          },
         }
       }
     }
@@ -20,3 +35,4 @@ M.setup = function(lsp_flags, capabilities, on_attach)
 end
 
 return M
+
