46d24d5509fd408aebd6bfb161d7272bf939738f
Edvard Englund <edvard.englund@medsci.uu.se>
2025-08-10 11:41:12 +0200

    Fix latest run config cache for post-analysis

--- lua/python/launch_post_analysis.lua
+++ lua/python/launch_post_analysis.lua
@@ -2,27 +2,25 @@ local Job = require("plenary.job")
 local M = {}
 local run_config_file_instance = ""
 local latest_run_setting = ""
-local latest_rundir = "C:\\post-analys\\20230414134119_P6_10085_CID-221931C0286\\RES_2025-05-27_170153\\Raw\\DataAnalyst\\2025-07-28_095634"
--- local latest_rundir = ""
+-- local latest_rundir = "C:\\post-analys\\20230414134119_P6_10085_CID-221931C0286\\RES_2025-05-27_170153\\Raw\\DataAnalyst\\2025-07-28_095634"
+local latest_rundir = ""
 
 M.launch = function()
   local run_settings = require'read-settings'.read_json('.command.json')
   P(run_settings["cmd"])
   cmd = run_settings["cmd"]
   local launch_text = "post analysis"
-  local instruction = { "hidden-scratch", cmd, { "RuntimeError" }, launch_text, "." }
+  local instruction = { "hidden-scratch", cmd, { "RuntimeError", "Traceback" }, launch_text, "." }
   local instructions = {instruction}
   require'trigger-commands'.run_poly( instructions )
 end
 
 local fetch_latest_run_setting = function()
+  local settings_table = require'read-settings'.read_json(vim.g.vim_settings_file) or {}
+  latest_run_setting = settings_table[vim.g.vim_settings_config_entry] or ""
   if latest_run_setting == "" then
-    local settings_table = require'read-settings'.read_json(vim.g.vim_settings_file) or {}
-    latest_run_setting = settings_table[vim.g.vim_settings_config_entry] or ""
-    if latest_run_setting == "" then
-      print("No latest run setting available, you have to choose a single run first!")
-      return
-    end
+    print("No latest run setting available, you have to choose a single run first!")
+    return
   end
 end
 
@@ -80,7 +78,7 @@ M.launch2 = function(run_config_file)
   local run_config = require'read-settings'.read_json(run_config_file)
   cmd = run_config["cmd"]
   local launch_text = "post analysis"
-  local instruction1 = { "hidden-scratch", cmd, { "RuntimeError" }, launch_text, "." }
+  local instruction1 = { "hidden-scratch", cmd, { "RuntimeError", "Traceback" }, launch_text, "." }
   local instruction2 = { "lua", copy_path_clipboard_wrapped, "Copy runpath" }
   local instructions = {instruction1, instruction2}
   require'trigger-commands'.run_poly( instructions )
