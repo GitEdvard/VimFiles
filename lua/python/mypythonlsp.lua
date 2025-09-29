d253431abe6a664b7d52cd1a3e20c5c0e0d388f5
Edvard Englund <edvard.englund@medsci.uu.se>
2025-09-29 14:39:54 +0200

    Show class family in telescope picker

--- lua/python/mypythonlsp.lua
+++ lua/python/mypythonlsp.lua
@@ -8,10 +8,17 @@ local finders = require "telescope.finders"
 local make_entry = require "telescope.make_entry"
 
 
-local query_for_class = [[
+local query_for_superclass = [[
 (
 (class_definition
 superclasses: (argument_list (identifier) @name))
+)
+]]
+
+local query_for_class = [[
+(
+(class_definition
+name: (identifier) @name)
 )
 ]]
 
@@ -21,6 +28,23 @@ local query_for_function = [[
 name: (identifier) @name)
 )
 ]]
+
+local execute_query = function(query_list)
+    local bufnr = vim.api.nvim_get_current_buf()
+    local search_hit = M.execute_query(bufnr, query_list, "python")
+    return search_hit
+end
+
+local find_with_rg_from_str = function(search_text)
+    local cwd = vim.fn.getcwd()
+    grepper = Job:new({
+      command = "rg",
+      args = {"--vimgrep", "--type", "py", "-w", search_text, cwd},
+      cwd = cwd,
+    })
+    local rg_hits = grepper:sync()
+    return rg_hits
+end
 
 local find_with_prefix = function(query_list, prefix)
     local bufnr = vim.api.nvim_get_current_buf()
@@ -39,6 +63,76 @@ local find_with_prefix = function(query_list, prefix)
     return rg_hits
 end
 
+
+local find_super_class_name = function()
+    local query_list = {
+        ['class'] = query_for_superclass,
+    }
+    return execute_query(query_list)
+end
+
+local find_super_class = function()
+    local query_list = {
+        ['class'] = query_for_superclass,
+    }
+    local prefix = "class "
+    return find_with_prefix(query_list, prefix)
+end
+
+local find_subclasses = function(super_class_name)
+    local search_text = "class .*?\\((.* ,)?\\b" ..  super_class_name .."\\b(, .*)?\\)"
+    local cwd = vim.fn.getcwd()
+    grepper = Job:new({
+      command = "rg",
+      args = {"--vimgrep", "--type", "py", "-e", search_text, cwd},
+      cwd = cwd,
+    })
+    local rg_hits = grepper:sync()
+    local filtered_hits = {}
+    for _, value in pairs(rg_hits) do
+      -- Match whole word only
+      if not value:find("%f[%a]tests%f[%A]") then
+        table.insert(filtered_hits, value)
+      end
+    end
+    return filtered_hits
+end
+
+find_subclasses_rec = function(root_class, rg_hits)
+  local hits = find_subclasses(root_class)
+  for _, single_rg_hit in pairs(hits) do
+    table.insert(rg_hits, single_rg_hit)
+    local class_name = single_rg_hit:match(".*class (.*)%(.*%).*")
+    local class_name_trimmed = class_name:gsub("%s+", "")
+    find_subclasses_rec(class_name_trimmed, rg_hits)
+  end
+  return rg_hits
+end
+
+local find_sibling_classes = function(query_list)
+    local bufnr = vim.api.nvim_get_current_buf()
+    local cwd = vim.fn.getcwd()
+    local search_hit = M.execute_query(bufnr, query_list, "python")
+    if search_hit == "" or search_hit == nil then
+      return {}
+    end
+    local search_text = "class .*?\\((.* ,)?\\b" ..  search_hit .."\\b(, .*)?\\)"
+    grepper = Job:new({
+      command = "rg",
+      args = {"--vimgrep", "--type", "py", "-e", search_text, cwd},
+      cwd = cwd,
+    })
+    local rg_hits = grepper:sync()
+    local filtered_hits = {}
+    for _, value in pairs(rg_hits) do
+      -- Match whole word "def" only
+      if not value:find("%f[%a]tests%f[%A]") then
+        table.insert(filtered_hits, value)
+      end
+    end
+    return filtered_hits
+end
+
 local find_filtered_for_def = function(query_list)
     local bufnr = vim.api.nvim_get_current_buf()
     local cwd = vim.fn.getcwd()
@@ -55,7 +149,8 @@ local find_filtered_for_def = function(query_list)
     local rg_hits = grepper:sync()
     local filtered_hits = {}
     for _, value in pairs(rg_hits) do
-      if not value:find("def") then
+      -- Match whole word "def" only
+      if not value:find("%f[%a]def%f[%A]") then
         table.insert(filtered_hits, value)
       end
     end
@@ -75,16 +170,6 @@ local find_method_usages = function()
         ['function'] = query_for_function,
     }
     return find_filtered_for_def(query_list)
-end
-
-local find_super_class = function()
-    local bufnr = vim.api.nvim_get_current_buf()
-    local cwd = vim.fn.getcwd()
-    local query_list = {
-        ['class'] = query_for_class,
-    }
-    local prefix = "class "
-    return find_with_prefix(query_list, prefix)
 end
 
 local to_vim_script_arr = function(lua_table)
@@ -111,6 +196,26 @@ local show_picker = function(title, contents_table)
   }):find()
 end
 
+N.find_current_class_name = function()
+    local query_list = {
+        ['class'] = query_for_class,
+    }
+    local bufnr = vim.api.nvim_get_current_buf()
+    local cwd = vim.fn.getcwd()
+    local search_hit = M.execute_query(bufnr, query_list, "python")
+    return search_hit
+end
+
+N.find_current_method_name = function()
+    local query_list = {
+        ['function'] = query_for_function,
+    }
+    local bufnr = vim.api.nvim_get_current_buf()
+    local cwd = vim.fn.getcwd()
+    local search_hit = M.execute_query(bufnr, query_list, "python")
+    return search_hit
+end
+
 N.show_method_definitions = function()
   local method_definitions = find_method_definitions()
   local opts = {}
@@ -128,6 +233,43 @@ N.goto_superclass = function()
   super_class_hits_arr = to_vim_script_arr(super_class_hits)
   vim.cmd { cmd = 'cgetexpr', args = {super_class_hits_arr} }
   vim.cmd { cmd = 'cfirst'}
+end
+
+local find_root_super_class_name = function()
+  local next_candidate_super_class = find_super_class_name()
+  local latest_super_class = ""
+  local latest_rg_hits = {}
+  local rg_hits = find_with_rg_from_str("class " .. next_candidate_super_class)
+  while #rg_hits > 0 do
+    latest_super_class = next_candidate_super_class
+    latest_rg_hits = rg_hits
+    first_rg_hit = rg_hits[1]
+    rg_hits = {}
+    local within_params = first_rg_hit:match("%((.*)%)")
+    local within_params_trimmed = within_params:gsub("%s+", "")
+    if not (within_params_trimmed == "") then
+      local class_list = mysplit(within_params, ",")
+      next_candidate_super_class = class_list[1]
+      rg_hits = find_with_rg_from_str("class " .. next_candidate_super_class)
+    end
+  end
+  return latest_super_class, latest_rg_hits
+end
+
+N.show_class_family = function()
+  local root_super_class_name, rg_hits_for_root = find_root_super_class_name()
+  local accumulated_hits = rg_hits_for_root
+  find_subclasses_rec(root_super_class_name, accumulated_hits)
+  show_picker("Class family", accumulated_hits)
+end
+
+-- remove
+N.show_sibbling_classes = function()
+    local query_list = {
+        ['class'] = query_for_superclass,
+    }
+  siblings = find_sibling_classes(query_list)
+  show_picker("Sibling classes", siblings)
 end
 
 return N
